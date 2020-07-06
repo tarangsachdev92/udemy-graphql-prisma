3af6eee7d495f8bc79a83ce64a3bb1a3f8397833
tarangSachdev92 <tarangsachdev@gmail.com>
2020-07-06 14:53:18 +0530

    pull jwt secret out of code

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (userId) => {
-    return jwt.sign({ userId }, 'thisisasecret', { expiresIn: '1 day' })
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1 day' })
 }
 
 export { generateToken as default }
